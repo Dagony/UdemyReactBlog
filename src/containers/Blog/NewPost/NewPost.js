bbd1baaf4759182a96647aecdadb0f60f4b3daf0
dagony <dagony@gmail.com>
2019-04-11 17:29:43 +0200

    Using the History Prop to Redirect (Replace)

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {Redirect} from 'react-router-dom';
 
 import './NewPost.css';
 import axios from "axios";
@@ -8,8 +7,7 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max',
-        submitted: false
+        author: 'Max'
     };
 
     componentDidMount() {
@@ -25,18 +23,13 @@ class NewPost extends Component {
         axios.post('/posts', post)
             .then(response => {
                 console.log(response);
-                this.setState({submitted: true})
+                this.props.history.push('/posts');
             });
     };
 
     render () {
-        let redirect = null;
-        if (this.state.submitted) {
-            redirect = <Redirect to={"/posts"} />;
-        }
         return (
             <div className="NewPost">
-                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
